99b052bcc904bf71302cdc97a8fc5b40cf7c1811
phone82 <phone82msa@gmail.com>
2020-11-03 16:59:18 +0900

    2nd commit

--- src/main/java/phoneseller/Payment.java
+++ src/main/java/phoneseller/Payment.java
@@ -2,6 +2,9 @@ package phoneseller;
 
 import javax.persistence.*;
 import org.springframework.beans.BeanUtils;
+import org.springframework.transaction.support.TransactionSynchronizationAdapter;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
 import java.util.List;
 
 @Entity
@@ -16,9 +19,10 @@ public class Payment {
     private String process;
 
     @PostPersist
-    public void onPostPersist(){
+    public void onPrePersist(){
         System.out.println("***** 결재 처리 중 *****");
         System.out.println(getId());
+        System.out.println(getOrderId());
         System.out.println(getProcess());
 
         if("Ordered".equals(process)) {
@@ -31,15 +35,33 @@ public class Payment {
             BeanUtils.copyProperties(this, payCompleted);
             payCompleted.publishAfterCommit();
 
+
+            //바로 이벤트를 보내버리면 주문정보가 커밋되기도 전에 배송발송됨 이벤트가 발송되어 주문테이블의 상태가 바뀌지 않을 수 있다.
+            // TX 리스너는 커밋이 완료된 후에 이벤트를 발생하도록 만들어준다.
+            TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronizationAdapter() {
+                @Override
+                public void beforeCommit(boolean readOnly) {
+                    System.out.println("published");
+                    payCompleted.publish();
+                }
+            });
+
             System.out.println("***** 결재 완료 *****");
 
-        } else {
+//            try {
+//                Thread.currentThread().sleep((long) (400 + Math.random() * 220));
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+
+
+        } else if("Order Cancel".equals(process)) {
             System.out.println("***** 결재 취소 중 *****");
 
             PayCancelled payCancelled = new PayCancelled();
             payCancelled.setId(getId());
             payCancelled.setOrderId(getOrderId());
-            payCancelled.setProcess("Payment Cancelled");
+            payCancelled.setProcess("Pay Cancelled");
             BeanUtils.copyProperties(this, payCancelled);
             payCancelled.publishAfterCommit();
         }
